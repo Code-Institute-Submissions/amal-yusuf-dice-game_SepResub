0116a8b5db00a4d493ac600cdb955d444b7388de
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-04-15 04:23:24 +0000

    Creates a class and method which holds our dice game functions

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Die:
+    """
+    Create instance of Die
+    """
+    def __init__(self, human_player, computer_player):
+        self.haman_palyer = human_player
+        self.computer_player = computer_player
+        self.score = 0
