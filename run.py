f6296fe778495caea7b497521bd4e7aee831ccb2
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-04-15 05:00:43 +0000

    Creates a while liip for the computer_move function similar to the human_move function

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ class Die:
     def dice(self):
         """Returns the rolled dice of random int between 1 and 6"""
         self.rollDice = random.randint(1, 6)
-        print(f"You rolled a", {self.rollDice})
+        # print(f"You rolled a", {self.rollDice})
 
     def human_move(self):
         """Creates a while loop for the human's turn"""
@@ -41,5 +41,28 @@ class Die:
         self.score += round_score
         print("Your turn is over")
         print(" your total score is  \n", {self.score})
+
+    def computer_move(self):
+        """Creates a while loop for the computer's turn"""
+        round_score = 0
+        rollagain = 'y'
+        while rollagain == 'y':
+            roll = self.rollDice 
+            if roll == 1:
+                print("Computer rolled a 1", roll)
+                round_score = 0
+                rollagain = 'n'
+            else:
+                print(f"Computer rolled a ", {self.rollDice})
+                round_score = round_score + self.rollDice
+                if round_score < 20:
+                    print("Computer will roll again")
+                else:
+                    rollagain = 'n'
+        self.score += round_score
+        print("Cumputer's turn is over")
+        print("Computer's round score is ", round_score)
+        print(f"Computer's total score is  \n", {self.score})
+
 
 
