77321729de125e2e83d6c5989335a382c7d95c1c
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-04-15 05:52:44 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ class Die:
         self.haman_palyer = human_player
         self.computer_player = computer_player
         self.score = 0
-    
+        
     def move(self):
         """
         Switches turn between players
@@ -17,7 +17,7 @@ class Die:
             self.human_move()
         else:
             self.computer_move()
-    
+
     def dice(self):
         """Returns the rolled dice of random int between 1 and 6"""
         self.rollDice = random.randint(1, 6)
@@ -27,8 +27,8 @@ class Die:
         """Creates a while loop for the human's turn"""
         round_score = 0
         rollagain = 'y'
-        while rollagain  == 'y':
-            roll = self.rollDice  
+        while rollagain == 'y':
+            roll = self.rollDice
             if roll == 1:
                 print('You rolled a 1', roll)
                 round_score = 0
@@ -47,7 +47,7 @@ class Die:
         round_score = 0
         rollagain = 'y'
         while rollagain == 'y':
-            roll = self.rollDice 
+            roll = self.rollDice
             if roll == 1:
                 print("Computer rolled a 1", roll)
                 round_score = 0
@@ -70,7 +70,7 @@ class Die:
     #     """
 
 
-#  The start_game variable is not defined yet 
+#  The start_game variable is not defined yet
 # start_game = Die("human_player", "computer_player")
 # start_game.play_game()
 # print(start_game.play_game())
@@ -94,9 +94,3 @@ class Die:
         start_game = Die("human_player", "compuer_player")
 
 StartGame()
-
-
-
-
-
-
