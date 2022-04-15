694a3c7b07e56cb76c52bb4b37fdc25956d0285a
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-04-15 04:27:43 +0000

    Creates a function that switch turns between a player and a computer

--- run.py
+++ run.py
@@ -8,3 +8,12 @@ class Die:
         self.haman_palyer = human_player
         self.computer_player = computer_player
         self.score = 0
+    
+    def move(self):
+        """
+        Switches turn between players
+        """
+        if self.human_player:
+            self.human_move()
+        else:
+            self.computer_move()
