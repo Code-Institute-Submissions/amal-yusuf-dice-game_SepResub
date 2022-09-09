6bb87ad4405a20954b597c0f8ab89c9b8053c7ad
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-09-09 19:16:04 +0000

    Deployment commit

--- run.py
+++ run.py
@@ -1,96 +1,142 @@
 import random
 
-class Die:
+
+class playermixin:
     """
-    Create instance of Die
-    """
-    def __init__(self, human_player, computer_player):
-        self.haman_palyer = human_player
-        self.computer_player = computer_player
+    Create instance of playermixin class have methods rolldice and hold
+     """
+    def __init__(self):
+        self.total = 0
         self.score = 0
-        
-    def move(self):
+
+    def rolldice(self):
         """
-        Switches turn between players
+        This method generate random number between 1 and 6
         """
-        if self.human_player:
-            self.human_move()
+        dienb = random.randint(1,6)
+        print("The player got " + str(dienb))  # print result
+        if dienb == 1:  # turn ends if player got 1
+            self.score = 0
+            return dienb
         else:
-            self.computer_move()
+            self.score = self.score + dienb   # else, add the die number to the score
+            return dienb
 
-    def dice(self):
-        """Returns the rolled dice of random int between 1 and 6"""
-        self.rollDice = random.randint(1, 6)
-        # print(f"You rolled a", {self.rollDice})
+    def hold(self):
+        """
+        only add the score to the total when the user chooses to hold
+        """
+        self.total = self.total+self.score
+        self.score = 0
 
-    def human_move(self):
-        """Creates a while loop for the human's turn"""
-        round_score = 0
-        rollagain = 'y'
-        while rollagain == 'y':
-            roll = self.rollDice
-            if roll == 1:
-                print('You rolled a 1', roll)
-                round_score = 0
-                rollagain = 'n'
+    def checkwin(self):
+        """
+        if 100 needs to be changed, also change in main code (after #STARTGAME)
+        """
+        if self.total > 100:
+            return True
+        else:
+            return False
+
+
+class human(playermixin):
+    """
+    Create instance of human class which has attributes: name, total,
+    turnscore and uses methods from playermixin class
+    """
+    def __init__(self,name):
+        """
+        inherits the super attributes, and has additional attribute name
+        """
+        super().__init__()
+        self.name = name
+
+    def doturn(self):
+        print("Now it is the turn of " + self.name)
+        while True:
+           # this while loop to repeat only if the user didn't
+           # give a valid input for roll or hold
+            move = input("Would you like to roll(r) die or hold(h)?")
+            if move == 'h':
+                super().hold()
+                break
+            elif move == 'r':
+                dienb = super().rolldice()
+                if dienb == 1:
+                    break
             else:
-                print(f"You rolled a ", {self.rollDice})
-                round_score = round_score + self.rollDice
-                print("Your's round score is ", round_score)
-                rollagain = input('Would like to roll again? (y/n)')
-        self.score += round_score
-        print("Your turn is over")
-        print(" your total score is  \n", {self.score})
-
-    def computer_move(self):
-        """Creates a while loop for the computer's turn"""
-        round_score = 0
-        rollagain = 'y'
-        while rollagain == 'y':
-            roll = self.rollDice
-            if roll == 1:
-                print("Computer rolled a 1", roll)
-                round_score = 0
-                rollagain = 'n'
-            else:
-                print(f"Computer rolled a ", {self.rollDice})
-                round_score = round_score + self.rollDice
-                if round_score < 20:
-                    print("Computer will roll again")
-                else:
-                    rollagain = 'n'
-        self.score += round_score
-        print("Cumputer's turn is over")
-        print("Computer's round score is ", round_score)
-        print(f"Computer's total score is  \n", {self.score})
-
-    # def play_game(self):
-    #     """
-    #     This function will manage the game and is not created yet
-    #     """
+                print("please input valid answer.")
 
 
-#  The start_game variable is not defined yet
-# start_game = Die("human_player", "computer_player")
-# start_game.play_game()
-# print(start_game.play_game())
+class cpu(playermixin):
+    """
+    Create instance of a cpu class which has attributes score, total, 
+    and uses methods from playermixin class
+    """
+    def __init__(self):
+        super().__init__()
 
-    def StartGame():
-        ''' Displays a welcome message for the game and start the game'''
-        human_score = 0
-        computer_score = 0
-        human_score < 100 and computer_score < 100:
-        print("-" * 35)
-        print("        ***     ")
-        print("Welcome to Dice Pig Game!")
-        print("        ***     ")
-        print("The current score is ")
-        print("Human_Player is : ", human_score)
-        print()
-        print("Computer is : ", computer_score)
-        print("-" * 35)
-        human_player = input("Please enter your name: \n")
-        print("-" * 35)
-        start_game = Die("human_player", "compuer_player")
+    def doturn(self):
+        print("Now it is the CPU's turn.")
+        while True:
+            move = random.randint(1,4) 
+            if move == 4:  # hold
+                print("CPU player will hold.")
+                super().hold()
+                print()
+                break
+            elif move < 4:  # rolldie
+                dienb = super().rolldice()
+                if dienb == 1:
+                    break
 
-StartGame()
+# STARTGAME
+print('-' * 40)
+print('Welcome to the Dice Pig Game!')
+print('-' * 40)
+
+# ask user if he wants to play against CPU or users
+game = input("Would you like to play against the CPU(c) or a human(h)? (c/h)")
+
+if game == 'c':  # user chooses cpu
+    name1 = input("Please insert your name:")
+    # both players initialised
+    player1 = human(name1)
+    cpu1 = cpu()
+
+    while True:
+        player1.doturn()
+        if player1.checkwin():   # checks if player1 has won, end game
+            print(player1.name+"  has won.")
+            break   # exit the while loop
+        cpu1.doturn()
+        if cpu1.checkwin():
+            print(player1.name+"  has lost.")
+            break
+elif game == 'h':
+    while True:
+        try:
+            nbplayers = int(input("How many players would you like?"))
+            # initialising list of players
+            players = {}
+            for i in range(nbplayers):
+                name = input("Please insert name of player " + str(i+1)+":")
+                players[i] = human(name)
+                """
+                initialise a human player and place him/her in the players list
+                """
+
+            while True:
+                for i in range(nbplayers):
+                    #  give each each player a turn
+                    players[i].doturn()
+                    if players[i].checkwin():
+                        print(players[i].name + " has won.")
+                        break
+                if players[i].checkwin():
+                    break
+            break
+        except:
+            print("Please input a number next time. Try again now.")
+else:
+    print("You inputted an invalid input.")
